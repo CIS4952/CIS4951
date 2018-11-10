91f74d0bc674092b15572d5d9c3b07fb1fe81737
GaruJPN <ytaya@umd.umich.edu>
2018-11-10 14:27:35 -0500

    Erased previous work
    Started on Login page

--- app/src/main/java/linkubank/linkubank/MainActivity.java
+++ app/src/main/java/linkubank/linkubank/MainActivity.java
@@ -8,28 +8,16 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-    public void onButtonClick(View v)
-    {
-        Double monthIncome, monthExpense, monthCashAvail, projectYearlyCash;
-        EditText edit1 = (EditText)findViewById(R.id.editText);
-        EditText edit2 = (EditText)findViewById(R.id.editText2);
-        TextView text1 = (TextView)findViewById(R.id.textView7);
-        TextView text2 = (TextView)findViewById(R.id.textView5);
-
-        monthIncome = Double.parseDouble(edit1.getText().toString());
-        monthExpense = Double.parseDouble(edit2.getText().toString());
-
-        monthCashAvail = monthIncome - monthExpense;
-        text1.setText(Double.toString(monthCashAvail));
-
-        projectYearlyCash = monthCashAvail * 12;
-        text2.setText(Double.toString(projectYearlyCash));
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the login button */
+    public void logIn(View view)
+    {
+
     }
 }
