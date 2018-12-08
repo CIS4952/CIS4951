da9381576f2a4e83df9ed6a857c73c4eefa2356b
GaruJPN <ytaya@umd.umich.edu>
2018-12-08 15:50:45 -0500

    Restarted project. Importing images etc from AdobeXD.

--- app/src/main/java/linkubank/linkubank/MainActivity.java
+++ app/src/main/java/linkubank/linkubank/MainActivity.java
@@ -9,26 +9,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-    private Button button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                //openActivity2();
-            }
-        });
-    }
-
-    /** Called when the user taps the login button */
-    public void logIn(View view) {
- //       Intent intent = new Intent(this, Activity2.class);
- //       startActivity(intent);
-        //code for new page
     }
 }
