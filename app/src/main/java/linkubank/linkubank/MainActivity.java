17f5b2988db00b291da7ae78e8e7601e001a1154
GaruJPN <ytaya@umd.umich.edu>
2018-12-09 16:16:21 -0500

    Login screen interfaces done. Needs codes to make actual login work.

--- app/src/main/java/linkubank/linkubank/MainActivity.java
+++ app/src/main/java/linkubank/linkubank/MainActivity.java
@@ -1,5 +1,6 @@
 package linkubank.linkubank;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,9 +10,25 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                logInApp();
+            }
+        });
+    }
+
+    /** Called when the user taps the Login button */
+    public void logInApp() {
+        Intent intent = new Intent(this, loginpage.class);
+        startActivity(intent);
     }
 }
