be9765362b26df95fddda0f95170dc6d2d46662f
GaruJPN <ytaya@umd.umich.edu>
2018-12-09 18:51:16 -0500

    Budget start page completed Linked to budget create option and link to professionals page

--- app/src/main/java/linkubank/linkubank/MainActivity.java
+++ app/src/main/java/linkubank/linkubank/MainActivity.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -17,7 +15,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
+        button = (Button) findViewById(R.id.btnLogin);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
--- app/src/main/java/linkubank/linkubank/budgetStartPage.java
+++ app/src/main/java/linkubank/linkubank/budgetStartPage.java
@@ -1,13 +1,47 @@
 package linkubank.linkubank;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class budgetStartPage extends AppCompatActivity {
+
+    private Button button1; //Initialization for btnLinkProf
+    private Button button2; //Initialization for btnCreateBudget
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget_start_page);
+
+        button1 = (Button) findViewById(R.id.btnLinkProf);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v1) {
+                linkToProf();
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.btnCreateBudget);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v2) {
+                budgetCreate();
+            }
+        });
+    }
+
+    //Code for opening Link to professional page
+    public void linkToProf() {
+        Intent intent1 = new Intent(this, linkProfessionals.class);
+        startActivity(intent1);
+    }
+
+    //Code for opening Budget creation page
+    public void budgetCreate() {
+        Intent intent2 = new Intent(this, budgetCreateOption.class);
+        startActivity(intent2);
     }
 }
