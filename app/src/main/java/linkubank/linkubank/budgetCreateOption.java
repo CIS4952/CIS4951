256a431441800d005f90b16765de5c9fd7d128e2
GaruJPN <ytaya@umd.umich.edu>
2018-12-11 00:27:05 -0500

    random update

--- app/src/main/java/linkubank/linkubank/budgetCreateOption.java
+++ app/src/main/java/linkubank/linkubank/budgetCreateOption.java
@@ -1,13 +1,31 @@
 package linkubank.linkubank;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class budgetCreateOption extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget_create_option);
+
+        button = (Button) findViewById(R.id.btnCreateManualBudget);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                budgetStartSelect();
+            }
+        });
+    }
+
+    public void budgetStartSelect() {
+        Intent intent = new Intent(this, budgetStartSelect.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/linkubank/linkubank/budgetStartSelect.java
@@ -0,0 +1,45 @@
+package linkubank.linkubank;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class budgetStartSelect extends AppCompatActivity {
+
+    private Button button1;
+    private Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budget_start_select);
+
+        button1 = (Button) findViewById(R.id.btnIncome);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v1) {
+                linkToIncome();
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.btnExpenses);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v2) {
+                linkToExpenses();
+            }
+        });
+    }
+
+    public void linkToIncome() {
+        Intent intent1 = new Intent(this, incomeSelection.class);
+        startActivity(intent1);
+    }
+
+    public void linkToExpenses() {
+        Intent intent2 = new Intent(this, expenseSelection.class);
+        startActivity(intent2);
+    }
+}
