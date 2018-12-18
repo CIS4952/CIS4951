10543ef04f4460b833e3f94b3147b20d7dc2f20d
GaruJPN <ytaya@umd.umich.edu>
2018-12-18 15:35:59 -0500

    income page interface done(?) save button links back to budgetStartSelect page

--- app/src/main/java/linkubank/linkubank/scrollingIncomeInputPage.java
+++ app/src/main/java/linkubank/linkubank/scrollingIncomeInputPage.java
@@ -1,13 +1,30 @@
 package linkubank.linkubank;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class scrollingIncomeInputPage extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling_income_input_page);
+
+        button = (Button) findViewById(R.id.btnSaveIncome);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v1) {
+                linkToBudgetStartSelect();
+            }
+        });
+    }
+
+    public void linkToBudgetStartSelect() {
+        Intent intent = new Intent(this, budgetStartSelect.class);
+        startActivity(intent);
     }
 }
