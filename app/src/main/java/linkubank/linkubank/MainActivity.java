b3bce4955ff292b487989c1d95daaa338be94815
GaruJPN <ytaya@umd.umich.edu>
2018-12-19 07:21:04 -0500

    qol changes

--- app/src/main/java/linkubank/linkubank/MainActivity.java
+++ app/src/main/java/linkubank/linkubank/MainActivity.java
@@ -1,31 +1,19 @@
 package linkubank.linkubank;
 
-import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-
-import java.lang.String;
-import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity
 {
-    EditText editUser, editPass;
-
     private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        editUser = (EditText)findViewById(R.id.editText);
-        editPass = (EditText)findViewById(R.id.editText3);
 
         button = (Button) findViewById(R.id.btnLogin);
         button.setOnClickListener(new View.OnClickListener(){
@@ -38,31 +26,7 @@ public class MainActivity extends AppCompatActivity
 
     /** Called when the user taps the Login button */
     public void logInApp() {
-
-        String username = editUser.getText().toString();
-        String password = editPass.getText().toString();
-        //String username = "TimIversen";
-        //String password = "Linkuban";
-        String type = "login";
-        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
-        try {
-            String result = backgroundWorker.execute(type, username, password).get();
-            if(result.equals("Login Success")) {
-                Intent intent = new Intent(this, budgetStartPage.class);
-                startActivity(intent);
-            }
-            else {
-                AlertDialog.Builder alertDialogBuilder =
-                        new AlertDialog.Builder(this)
-                                .setTitle("Login Status")
-                                .setMessage("Login Failed");
-
-                AlertDialog alertDialog = alertDialogBuilder.show();
-            }
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Intent intent = new Intent(this, budgetStartPage.class);
+        startActivity(intent);
     }
 }
